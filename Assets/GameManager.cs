9620b86c50e9bd582c59e5ce9316a3e02c0b614b
Chris Crain <chrisac309@gmail.com>
2019-11-16 16:23:20 -0500

    Prefab generation progress

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,26 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject LevelPrefab;
-
-    private Vector3 referencePosition;
-    private float levelPrefabWidth;
+    private GameObject[] prefabArray;
+    private Transform cameraTransform;
+    private float referencePosition;
+    private float shiftDistance;
+    private float cameraWidth;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // Using the levelPrefabWidth and the referencePosition, we can determine when to 
+        // Using the levelcameraWidth and the referencePosition, we can determine when to 
         // move one of the three (or five) level prefab objects in front of the player
-        referencePosition = Camera.main.transform.position;
+        cameraTransform = Camera.main.transform; 
+        referencePosition = cameraTransform.position.x;
+        cameraWidth = Camera.main.orthographicSize;
+        shiftDistance =  cameraWidth * 5;
+        initializePrefabArray();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        referencePosition = cameraTransform.position.x;
+    }
+
+    private void initializePrefabArray() {
+        prefabArray = new GameObject[5];
+        prefabArray[0] = Instantiate(LevelPrefab, new Vector3(0, 0), Quaternion.identity);
+        prefabArray[1] = Instantiate(LevelPrefab, new Vector3(cameraWidth, 0), Quaternion.identity);
+        prefabArray[2] = Instantiate(LevelPrefab, new Vector3(cameraWidth * 2, 0), Quaternion.identity);
+        prefabArray[3] = Instantiate(LevelPrefab, new Vector3(cameraWidth * 3, 0), Quaternion.identity);
+        prefabArray[4] = Instantiate(LevelPrefab, new Vector3(cameraWidth * 4, 0), Quaternion.identity);
+    }
+
+    private void shiftPrefabAt(int position) {
+        var pos = prefabArray[position].transform.position;
+        var shift = pos.x + 5 * cameraWidth;
+        prefabArray[position].transform.position = new Vector3(shift, pos.y);
     }
 }
